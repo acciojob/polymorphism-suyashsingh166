32711c7a45feec266b3c42b48e45e165b3a84a33
Suyash Singh <112690036+suyashsingh166@users.noreply.github.com>
2023-03-17 11:28:34 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,18 @@
 package com.driver;
 import java.util.*;
 public class Main {
+  
+   
   public class Product{
+    int x,y,z;
+   double a,b;
+     public Product(int x,int y,int z,double a,double b)
+     {
+       this.x=x;
+       this.y=y;
+       this.a=a;
+       this.b=b;
+     }
     public int product(int x, int y) {
     return x*y;
     }
@@ -12,20 +23,15 @@ public class Main {
      return x*y;
      }
     
-  }
+  
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
    Product p=new Product();
-    int x=sc.nextInt();
-    int y=sc.nextInt();
-    int z=sc.nextInt();
-    double x=sc.nextDouble();
-    double y=sc.nextDouble();
+    
     p.product(x,y);
     p.product(x,y,z);
-    p.product(x,y);
+    p.product(a,b);
   }
-  
-
+}
 }
