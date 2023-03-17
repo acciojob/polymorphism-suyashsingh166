f2e87c7f0c1b0e26e00b238a2da53f271f2e8e08
Suyash Singh <112690036+suyashsingh166@users.noreply.github.com>
2023-03-17 16:30:35 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,37 +1,33 @@
 package com.driver;
 import java.util.*;
 public class Main {
-  
-   
-  public class Product{
-    int x,y,z;
-   double a,b;
-     public Product(int x,int y,int z,double a,double b)
-     {
-       this.x=x;
-       this.y=y;
-       this.a=a;
-       this.b=b;
-     }
-    public int product(int x, int y) {
-    return x*y;
+static class Product{
+
+    public static void prod(int x, int y) {
+    System.out.println(x*y);
     }
-    public int product(int x, int y, int z) {
-    return x*y*z;
+    public static void prod(int x, int y, int z) {
+    System.out.println(x*y*z);
     }
-     public double product(double x, double y) {
-     return x*y;
-     }
+     public static void prod(double a, double b) {
+    System.out.println(a*b);
+     } 
     
-  
+  }
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
-   Product p=new Product();
-    
-    p.product(x,y);
-    p.product(x,y,z);
-    p.product(a,b);
+    int x,y,z;
+   double a,b;
+  
+   x=sc.nextInt();
+   y=sc.nextInt();
+   z=sc.nextInt();
+   a=sc.nextDouble();
+   b=sc.nextDouble();
+    Product.prod(x,y);
+    Product.prod(x,y,z);
+    Product.prod(a,b);
+   
   }
-}
 }
